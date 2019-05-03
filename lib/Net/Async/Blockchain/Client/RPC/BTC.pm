905d49a4d20be4054eb9f36e1238aa6a4ea2bc7e
regcostajr <regcostajr@gmail.com>
2019-05-03 03:50:14 +0000

    fix omni rpc calls

--- lib/Net/Async/Blockchain/Client/RPC/BTC.pm
+++ lib/Net/Async/Blockchain/Client/RPC/BTC.pm
@@ -17,7 +17,7 @@ Net::Async::Blockchain::Client::RPC::BTC - Async BTC RPC Client.
         my $http_client = Net::Async::Blockchain::Client::RPC::BTC->new(endpoint => 'http://127.0.0.1:8332', timeout => 100)
     );
 
-    my $response = $http_client->accounts()->get;
+    my $response = $http_client->get_transaction('txid...')->get;
 
 =head1 DESCRIPTION
 
@@ -50,6 +50,25 @@ sub get_transaction {
     return $self->_request('gettransaction', @params);
 }
 
+use parent qw(Net::Async::Blockchain::Client::RPC);
+
+=head2 get_raw_transaction
+
+https://bitcoin-rpc.github.io/en/doc/0.17.99/rpc/rawtransactions/getrawtransaction/
+
+=over 4
+
+=back
+
+L<Future>
+
+=cut
+
+sub get_raw_transaction {
+    my ($self, @params) = @_;
+    return $self->_request('getrawtransaction', @params);
+}
+
 =head2 get_block
 
 https://bitcoin-rpc.github.io/en/doc/0.17.99/rpc/wallet/getblock/
@@ -65,6 +84,23 @@ L<Future>
 sub get_block {
     my ($self, @params) = @_;
     return $self->_request('getblock', @params);
+}
+
+=head2 validate_address
+
+https://bitcoin-rpc.github.io/en/doc/0.17.99/rpc/util/validateaddress/
+
+=over 4
+
+=back
+
+L<Future>
+
+=cut
+
+sub validate_address {
+    my ($self, @params) = @_;
+    return $self->_request('validateaddress', @params);
 }
 
 1;
--- lib/Net/Async/Blockchain/Omni.pm
+++ lib/Net/Async/Blockchain/Omni.pm
@@ -36,11 +36,14 @@ Omnicore subscription using ZMQ from the bitcoin based blockchain nodes
 no indirect;
 
 use Ryu::Async;
+use Future;
 use Future::AsyncAwait;
 use IO::Async::Loop;
 use Math::BigFloat;
+use Syntax::Keyword::Try;
 
 use Net::Async::Blockchain::Transaction;
+use Net::Async::Blockchain::Client::RPC::Omni;
 
 use parent qw(Net::Async::Blockchain::BTC);
 
@@ -48,6 +51,57 @@ use parent qw(Net::Async::Blockchain::BTC);
 use constant FEE_CURRENCY => 'BTC';
 
 my %subscription_dictionary = ('transactions' => 'hashblock');
+
+=head2 rpc_client
+
+Create an L<Net::Async::Blockchain::Client::RPC> instance, if it is already defined just return
+the object
+
+=over 4
+
+=back
+
+L<Net::Async::Blockchain::Client::RPC>
+
+=cut
+
+sub rpc_client : method {
+    my ($self) = @_;
+    return $self->{rpc_client} //= do {
+        $self->add_child(my $http_client = Net::Async::Blockchain::Client::RPC::Omni->new(endpoint => $self->rpc_url));
+        $self->{rpc_client} = $http_client;
+        return $self->{rpc_client};
+        }
+}
+
+=head2 hashblock
+
+hashblock subscription
+
+Convert and emit a L<Net::Async::Blockchain::Transaction> for the client source every new block received that
+is owned by the node.
+
+=over 4
+
+=item * C<block_hash> omnicore block hash
+
+=back
+
+=cut
+
+async sub hashblock {
+    my ($self, $block_hash) = @_;
+
+    my $block_response = await $self->rpc_client->get_block($block_hash);
+
+    # 2 here for full verbosity
+    my @future_transactions = map { $self->rpc_client->get_raw_transaction($_, 2) } $block_response->{tx}->@*;
+    await Future->needs_all(@future_transactions);
+
+    my @transactions = map {$_->get} @future_transactions;
+
+    await Future->needs_all(map { $self->transform_transaction($_) } @transactions);
+}
 
 =head2 transform_transaction
 
@@ -68,25 +122,32 @@ async sub transform_transaction {
 
     # the command listtransactions will guarantee that this transactions is from or to one
     # of the node addresses.
-    my $received_transaction = await $self->rpc_client->omni_gettransaction($decoded_raw_transaction->{txid});
+    my $received_transaction;
+    try {
+        $received_transaction = await $self->rpc_client->get_transaction($decoded_raw_transaction->{txid});
+    }
+    catch {
+        # transaction not found
+        return undef;
+    };
 
     # transaction not found, just ignore.
-    return undef unless $received_transaction && $received_transaction->{is_mine};
+    return undef unless $received_transaction && $received_transaction->{ismine};
 
     my $amount = Math::BigFloat->new($received_transaction->{amount});
     my $fee = Math::BigFloat->new($received_transaction->{fee} // 0);
 
-    my $from = $self->rpc_client->validateaddress($received_transaction->{sendingaddress});
-    my $to   = $self->rpc_client->validateaddress($received_transaction->{referenceaddress});
+    my $from = await $self->rpc_client->validate_address($received_transaction->{sendingaddress});
+    my $to   = await $self->rpc_client->validate_address($received_transaction->{referenceaddress});
 
     # it can be receive, sent, internal
     # if categories has send and receive it means that is an internal transaction
     my $transaction_type;
-    if ($from->{is_mine} && $to->{is_mine}) {
+    if ($from->{ismine} && $to->{ismine}) {
         $transaction_type = 'internal';
-    } elsif ($from->{is_mine}) {
+    } elsif ($from->{ismine}) {
         $transaction_type = 'sent';
-    } elsif ($to->{is_mine}) {
+    } elsif ($to->{ismine}) {
         $transaction_type = 'receive';
     }
 
@@ -95,7 +156,7 @@ async sub transform_transaction {
     my $transaction = Net::Async::Blockchain::Transaction->new(
         currency     => $self->currency_symbol,
         hash         => $decoded_raw_transaction->{txid},
-        block        => $decoded_raw_transaction->{block},
+        block        => $received_transaction->{block},
         from         => $from->{address},
         to           => [$to->{address}],
         amount       => $amount,
