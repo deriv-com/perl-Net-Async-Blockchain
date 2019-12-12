27da8670bf4bf7830cdf3486eb3fbf3f220d08f6
Binary-Mohanad <mohanad@binary.com>
2019-12-12 04:07:24 +0000

    on_fail

--- lib/Net/Async/Blockchain/BTC.pm
+++ lib/Net/Async/Blockchain/BTC.pm
@@ -124,8 +124,8 @@ sub subscribe {
 
     die "Invalid or not implemented subscription" unless $subscription && $self->can($subscription);
     Future->needs_all(
-        $self->new_zmq_client->subscribe($subscription)->map(async sub { await $self->$subscription(shift) })->ordered_futures->completed(),
-        $self->recursive_search())->on_fail(sub { $self->source->fail(@_); });
+        $self->new_zmq_client->subscribe($subscription)->map(async sub { await $self->$subscription(shift) })->ordered_futures->completed()->on_fail(sub { $self->source->fail(@_); }),
+        $self->recursive_search());
 
     return $self->source;
 }
