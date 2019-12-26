c01eea6d8558e99fcedf1ecbc148cad73b946b91
reginaldo-binary <reginaldo@regentmartkets.com>
2019-12-26 12:02:56 +0800

    update

--- examples/eth_subscription.pl
+++ examples/eth_subscription.pl
@@ -13,10 +13,9 @@ my $loop = IO::Async::Loop->new;
 
 $loop->add(
     my $eth_client = Net::Async::Blockchain::ETH->new(
-        subscription_url  => "ws://127.0.0.1:8546",
-        rpc_url           => "http://127.0.0.1:8545",
-        base_block_number => 9008914
+        subscription_url => "ws://127.0.0.1:8546",
+        rpc_url          => "http://127.0.0.1:8545",
     ));
 
-$eth_client->subscribe("transactions")->each(sub { })->get();
+$eth_client->subscribe("transactions")->each(sub { print Dumper shift })->get();
 
